fff4352f8814b35eb7fb86e5ff5844e781602aa3
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-02-28 12:20:26 -0800

    fixed codeclimate issues

--- update_concourse.sh
+++ update_concourse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # gce-instance-resource update_concourse.sh
 
-fly -t ci set-pipeline -p gce-instance-resource -c ci/pipeline.yml --load-vars-from ../.credentials.yml
+fly -t ci set-pipeline -p gce-instance-resource -c ci/pipeline.yml --load-vars-from ../../../../.credentials.yml
